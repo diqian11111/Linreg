9ef9e4550c9949f794dbf06d0769a4f05d90bcf0
diqian11111 <diqian11111@gmail.com>
2017-11-09 00:36:25 -0500

    change files

--- tests/testthat/test_coef.R
+++ tests/testthat/test_coef.R
@@ -3,5 +3,5 @@ l1 <- linmod(Hwt ~ Bwt * Sex, data = cats)
 l2 <- lm(Hwt ~ Bwt * Sex, data = cats)
 
 test_that("same estimated coefficients as lm function", {
-  expect_equal(round(l1$coefficients, 3), round(l2$coefficients, 3))
+  expect_equal(as.numeric(round(l1$coefficients, 2)), as.numeric(round(l2$coefficients, 2)))
 })
