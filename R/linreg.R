c799c23bb7507311bd37d6b889f8a51dadae0cea
diqian11111 <diqian11111@gmail.com>
2017-11-15 23:57:10 -0500

    test

--- R/linreg.R
+++ R/linreg.R
@@ -33,51 +33,7 @@ linmodEst <- function(x, y) {
 }
 
 
-#' Linear regression
-#'
-#' Runs an OLS regression not unlike \code{\link{lm}}
-#'
-#' @param X covariate matrix (p x n) with no intercept
-#'
-#' @return A list with 4 elements: coefficients, vcov, sigma, df
-#'
-#' @examples
-#' data(mtcars)
-#' X <- as.matrix(mtcars[, c("cyl", "disp", "hp")])
-#' y <- mtcars[, "mpg"]
-#' linmodEst(y, X)
-#'
-#' @export
-#'
-linmod <- function(x, ...)
-  UseMethod("linmod")
 
-#' Linear regression
-#'
-#' Runs an OLS regression not unlike \code{\link{lm}}
-#'
-#' @param X covariate matrix (p x n) with no intercept
-#'
-#' @return A list with 4 elements: coefficients, vcov, sigma, df
-#'
-#' @examples
-#' data(mtcars)
-#' X <- as.matrix(mtcars[, c("cyl", "disp", "hp")])
-#' y <- mtcars[, "mpg"]
-#' linmodEst(y, X)
-#'
-#' @export
-#'
-linmod.default <- function(x, y, ...) {
-  x <- as.matrix(x)
-  y <- as.numeric(y)
-  est <- linmodEst(x, y)
-  est$fitted.values <- as.vector(x %*% est$coefficients)
-  est$residuals <- y - est$fitted.values
-  est$call <- match.call()
-  class(est) <- "linmod"
-  return(est)
-}
 
 #' Linear regression
 #'
